6bff9615b2b843b29fa3717260c75647afb04c84
Alexandr Kyshinko <kyshinkoalex@gmail.com>
2021-09-09 11:46:25 +0300

    change file

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,89 @@
+'use strict'
+
 // Задание 1
+
 function getArrayParams(arr) {
-  let min,max,sum,avg;
+  let min = Infinity;
+  let max = -Infinity;
+  let sum = 0;
+  let avg = 0;
 
-  // Ваш код
+  for ( let i = 0; i < arr.length; i++) {
+    if ( arr[i] > max) {
+      max = arr[i];
+    }   
+    
+    if ( arr[i] < min) {
+      min = arr[i];
+    }
 
-  return { min:min, max:max, avg:avg };
+    sum += arr[i];
+  }
+
+  avg = (sum / arr.length).toFixed(2);
+
+  return { min:min, max:max, avg:Number(avg) };
 }
 
-// Задание 2
-function worker(arr) {
-  let sum;
+console.log(getArrayParams([-99, 99, 10]));
 
-  // Ваш код
+
+
+// Задание 2
+
+function worker(arr) {
+  let sum = 0;
+
+  for ( let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
 
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max
+  let max = -Infinity;
 
-  // Ваш кода
-  // for ...
+    for ( let i = 0; i < arrOfArr.length; i++) {
+      let sum = func(arrOfArr[i]);
+      
+      console.log(sum);
+
+      if ( sum > max) {
+        max = sum;
+      }
+    }
   
-  return max
+  return max;
 }
 
+function mincer(arrOfArr) {
+  return makeWork(arrOfArr, worker);
+}
+
+
+
 // Задание 3
+
 function worker2(arr) {
-  // Ваш код
+  let min = Infinity;
+  let max = -Infinity;
+  let dif = 0;
+
+  for ( let i = 0; i < arr.length; i++) {
+    if ( arr[i] > max) {
+      max = arr[i];
+    }   
+    
+    if ( arr[i] < min) {
+      min = arr[i];
+    }
+  }
+  dif = max - min;
+
+  return dif;
+}
+
+function mincer2(arrOfArr) {
+  return makeWork(arrOfArr, worker2);
 }
